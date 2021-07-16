3c4a30f9c688f735ae2a4b72a901ccdc9b6fa203
Sairion350 <76807368+Sairion350@users.noreply.github.com>
2021-07-15 19:59:48 -0500

    GetAttatchedScripts

--- src/Papyrus/BaseObject.h
+++ src/Papyrus/BaseObject.h
@@ -7,6 +7,8 @@ namespace BaseObject
 
     //auto const formType = RE::FormType::Quest;
     auto const formType = RE::FormType::Script;
+
+    std::vector<RE::BSTSmartPointer<RE::BSScript::Object>> GetAttatchedScripts (RE::TESForm* form);
 	
 	RE::TESForm* MakeNewObject(RE::StaticFunctionTag*, std::string classtype)
 	{
@@ -34,23 +36,54 @@ namespace BaseObject
 
 		auto handle = virtualmachine->GetObjectHandlePolicy1()->GetHandleForObject(formType, form);
 
-		//virtualmachine->ResetAllBoundObjects(handle);
-		virtualmachine->GetObjectBindPolicy()->bindInterface->RemoveAllBoundObjects(handle);
+		virtualmachine->ResetAllBoundObjects(handle); // reset script vars
+		//virtualmachine->GetObjectBindPolicy()->bindInterface->RemoveAllBoundObjects(handle); // does nothing?
 		
 		// dangerous??
 		delete(form);
+
 	}
 
 	void Scan(RE::StaticFunctionTag*, RE::TESForm* form)
 	{
 
+		auto scripts = BaseObject::GetAttatchedScripts(form);
+
+		logger::info("{} script attatched", scripts.size());
+
+		
+	}
+
+	std::vector<RE::BSTSmartPointer<RE::BSScript::Object>> GetAttatchedScripts(RE::TESForm* form){
+		std::vector<RE::BSTSmartPointer<RE::BSScript::Object>> ret;
+
 		auto virtualmachine = RE::BSScript::Internal::VirtualMachine::GetSingleton();
 
 		auto handle = virtualmachine->GetObjectHandlePolicy1()->GetHandleForObject(formType, form);
 
-		//virtualmachine->GetObjectBindPolicy()->bindInterface->RemoveAllBoundObjects(handle);
+		auto map = &virtualmachine->attachedScripts;
 		
-		
+		RE::BSTSmallSharedArray<RE::BSScript::Internal::AttachedScript>* attachedscripts = nullptr;
+		for (auto& [vmhandle,scripts] : *map) { // todo, better way to get this? this type is giving me hell
+    		if (handle == vmhandle){
+    			attachedscripts = &scripts;
+    		}
+			
+		}
+
+		if (attachedscripts == nullptr){
+			return ret;
+		}
+
+		for (auto& i : *attachedscripts){
+			RE::BSTSmartPointer<RE::BSScript::Object> result;
+
+			virtualmachine->FindBoundObject(handle, i->GetTypeInfo()->name.c_str(), result);
+
+			ret.push_back(result);
+		}
+
+		return ret;
 	}
 
 
